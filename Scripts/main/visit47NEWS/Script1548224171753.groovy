4f2955522f7aa5938f86a77cddcfde49fa5a10be
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2019-01-23 15:41:10 +0900

    visiting 47NEWS

---
+++ Scripts/main/visit47NEWS/Script1548224171753.groovy
@@ -0,0 +1,46 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import java.nio.file.Files as Files
+import java.nio.file.Path as Path
+import java.nio.file.Paths as Paths
+import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kazurayam.materials.MaterialRepository
+import internal.GlobalVariable as GlobalVariable
+
+MaterialRepository mr = (MaterialRepository)GlobalVariable.MATERIAL_REPOSITORY
+
+// resolve output dir
+URL url = new URL('https://www.47news.jp/')
+
+// open browser
+WebUI.openBrowser('')
+
+// give tall enough view port as the AUT uses a fixed/sticky footer
+WebUI.setViewPortSize(1280, 3400)
+
+// navigate to the Google form page
+WebUI.navigateToUrl(url.toExternalForm())
+
+WebUI.verifyElementPresent(findTestObject('47NEWS/a_TOP'), 10)
+
+// take screen shot and save it into search_form.png file
+/*
+Path fileNamedByURL = mr.resolveScreenshotPath(GlobalVariable.CURRENT_TESTCASE_ID, url)
+CustomKeywords.'com.kazurayam.ksbackyard.ScreenshotDriver.saveEntirePageImage'(
+	fileNamedByURL.toFile())
+ */
+Path fileNamedFixed = mr.resolveMaterialPath(GlobalVariable.CURRENT_TESTCASE_ID,
+	"47NEWS_TOP.png")
+CustomKeywords.'com.kazurayam.ksbackyard.ScreenshotDriver.saveEntirePageImage'(
+	fileNamedFixed.toFile())
+
+// close browser
+WebUI.closeBrowser()
---
+++ Test Suites/Monitor47News.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Monitor47News</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>73449b19-1840-467f-9fa8-79628a4a958f</testSuiteGuid>
+   <testCaseLink>
+      <guid>42cfbf79-67a0-400a-b228-631b02b79737</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/main/visit47NEWS</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
