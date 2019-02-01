82d069c63c5bdb3f1743c14bdcbcff8e2b5f7d87
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2019-02-01 11:43:52 +0900

    Test Cases/utils/listRepository now works; Test Cases/utils/listStorage now works

--- Scripts/utils/listStorage/Script1548988054942.groovy
+++ Scripts/utils/listStorage/Script1548988054942.groovy
@@ -1,15 +1,23 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kazurayam.materials.Material
+import com.kazurayam.materials.MaterialStorage
+import com.kazurayam.materials.model.TSuiteResult
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
 import internal.GlobalVariable as GlobalVariable
 
+/**
+ * listStorage
+ */
+MaterialStorage ms = (MaterialStorage)GlobalVariable.MATERIAL_STORAGE
+
+List<TSuiteResult> tsrList = ms.getTSuiteResultList()
+
+def count = 0
+for (TSuiteResult tsr : tsrList) {
+	List<Material> mateList = tsr.getMaterialList()
+	for (Material mate : mateList) {
+		WebUI.comment("${mate.getPath().toString()}")
+		count += 1
+	}
+}
+WebUI.comment("number of Material files: ${count}")
