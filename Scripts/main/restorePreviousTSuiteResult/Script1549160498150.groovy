34245424a080685b26043bdb8da0786c3edf5d56
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2019-02-04 17:30:48 +0900

    update

--- Scripts/main/restorePreviousTSuiteResult/Script1549160498150.groovy
+++ Scripts/main/restorePreviousTSuiteResult/Script1549160498150.groovy
@@ -15,6 +15,9 @@ import java.time.LocalDateTime
 /**
  * restore the previous TSuiteResult from the Storage dir into the Materials dir
  */
+
+STRATEGY = 'last'    // last, 1hourAgo, lastEveningAt18
+
 MaterialRepository mr = (MaterialRepository)GlobalVariable.MATERIAL_REPOSITORY
 MaterialStorage    ms = (MaterialStorage)   GlobalVariable.MATERIAL_STORAGE
 TSuiteName tsn        = new TSuiteName(mr.getCurrentTestSuiteId()) 
@@ -27,10 +30,17 @@ switch(STRATEGY) {
 		// restore tha last shot previous to the current
 		count = ms.restoreUnary(mr, tsn, RetrievalBy.before(tst))
 		break
+		
 	case '1hourAgo':
 		// restore the shot of 1 hour ago or older
 		LocalDateTime base = LocalDateTime.now().minusHours(1)
 		count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base))
+		break
+		
+	case 'lastEveningAt18':
+	    // restore the shot of last evenng 18:00 or older
+	    LocalDateTime base = LocalDateTime.now().minusDays(1)
+		count = ms.restoreUnary(mr, tsn, RetrievalBy.before(base, 18, 0, 0))
 		break
 	default:
 		KeywordUtil.markFailedAndStop("unknown STRATEGY: ${STRATEGY}")
